fa0ba74f8e8489e5e96b705eac034cedf8691ab0
Michael Kotliar <misha.kotliar@gmail.com>
2018-05-19 19:34:39 -0400

    Update experimenttype2plugintype view

--- biowardrobe_airflow_plugins/sql_patch/biowardrobe_tables/experimentype2plugintype_view.sql
+++ biowardrobe_airflow_plugins/sql_patch/biowardrobe_tables/experimentype2plugintype_view.sql
@@ -1,9 +1,26 @@
 DROP VIEW IF EXISTS `ems`.`experimenttype2plugintype`;
 CREATE VIEW `ems`.`experimenttype2plugintype` AS
 
-# Assign Super Enhancer plugin to DNA-Seq experiment
 SELECT
     `e`.`id` AS `experimenttype_id`,
     `p`.`id` AS `plugintype_id`
 FROM  ((`ems`.`experimenttype` `e` JOIN `ems`.`plugintype` `p`))
 WHERE ((`e`.`etype`='DNA-Seq') AND (`p`.`etype`='Super Enhancer'))
+UNION
+SELECT
+    `e`.`id` AS `experimenttype_id`,
+    `p`.`id` AS `plugintype_id`
+FROM  ((`ems`.`experimenttype` `e` JOIN `ems`.`plugintype` `p`))
+WHERE ((`e`.`etype`='DNA-Seq pair') AND (`p`.`etype`='Super Enhancer'))
+UNION
+SELECT
+    `e`.`id` AS `experimenttype_id`,
+    `p`.`id` AS `plugintype_id`
+FROM  ((`ems`.`experimenttype` `e` JOIN `ems`.`plugintype` `p`))
+WHERE ((`e`.`etype`='DNA-Seq Trim Galore') AND (`p`.`etype`='Super Enhancer'))
+UNION
+SELECT
+    `e`.`id` AS `experimenttype_id`,
+    `p`.`id` AS `plugintype_id`
+FROM  ((`ems`.`experimenttype` `e` JOIN `ems`.`plugintype` `p`))
+WHERE ((`e`.`etype`='DNA-Seq pair Trim Galore') AND (`p`.`etype`='Super Enhancer'))
