491cfb5342bc05f567a58c4974ec43b93f39ba99
Michael Kotliar <misha.kotliar@gmail.com>
2018-05-19 19:04:01 -0400

    Add apply_sql_patch function locally

--- biowardrobe_airflow_plugins/biowardrobe_plugin_init.py
+++ biowardrobe_airflow_plugins/biowardrobe_plugin_init.py
@@ -1,6 +1,24 @@
 #! /usr/bin/env python3
 import sys
-from biowardrobe_airflow_analysis.biowardrobe_init import apply_sql_patch
+import os
+from sqlparse import split
+from biowardrobe_airflow_analysis.biowardrobe_init import _settings
+
+
+def apply_sql_patch(file):
+    sql_folder = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "sql_patch"))
+    file_abs_path = os.path.join(sql_folder, "biowardrobe_tables", file)
+    print(file_abs_path)
+    with open(file_abs_path) as ef:
+        for _sql in split(ef.read()):
+            if not _sql:
+                continue
+            try:
+                _settings.cursor.execute(_sql)
+                _settings.conn.commit()
+            except Exception as e:
+                print("Error: {}".format(e))
+                pass
 
 
 def main():
