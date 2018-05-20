6892764d763ca37a610400d55f1db016c3db6038
Michael Kotliar <misha.kotliar@gmail.com>
2018-05-20 08:20:25 -0400

    Change order of runnins sql scripts

--- biowardrobe_airflow_plugins/biowardrobe_plugin_init.py
+++ biowardrobe_airflow_plugins/biowardrobe_plugin_init.py
@@ -23,8 +23,8 @@ def apply_sql_patch(file):
 
 def main():
     try:
+        apply_sql_patch("plugintype_patch.sql")
         apply_sql_patch ("experimentype2plugintype_view.sql")
-        apply_sql_patch ("plugintype_patch.sql")
     except Exception as ex:
         print ("Failed to run SQL patches", ex.str())
         sys.exit(1)
