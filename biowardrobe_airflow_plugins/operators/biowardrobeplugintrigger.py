bc2d6e9ab9851602909b8367aab149ff963aacd8
Michael Kotliar <misha.kotliar@gmail.com>
2018-06-14 18:29:49 -0400

    Don't call sys.exit from Task
    Probbly it causes this task to fail

--- biowardrobe_airflow_plugins/operators/biowardrobeplugintrigger.py
+++ biowardrobe_airflow_plugins/operators/biowardrobeplugintrigger.py
@@ -25,22 +25,21 @@ class BioWardrobePluginTrigger(BaseOperator):
         if active_plugins:
             for active_plugin in active_plugins:
                 logger.info(f"""Active plugins found for {uid}:\n{active_plugin['dag_id']} - {active_plugin['run_id']}""")
-            sys.exit(0)
-
-        for plugin in plugins:
-            try:
-                dag_id = os.path.splitext(plugin['workflow'])[0].replace(".", "_dot_")
-                run_id = "__".join(["trig", uid, str(uuid.uuid4())])
-                payload = {'uid': uid, 'run_id': run_id}
-                session = settings.Session()
-                dr = DagRun(dag_id=dag_id,
-                            run_id=run_id,
-                            conf=payload,
-                            execution_date=datetime.now(),
-                            external_trigger=True)
-                session.add(dr)
-                session.commit()
-                session.close()
-                logger.info(f"""Trigger {plugin["ptype"]} ({dag_id}) plugin for {uid}:\nrun_id: {run_id}""")
-            except SQLAlchemyError as err:
-                logger.error(f"""Failed to trigger {plugin["ptype"]} ({dag_id}) plugin for {uid}\n {err}""")
+        else:
+            for plugin in plugins:
+                try:
+                    dag_id = os.path.splitext(plugin['workflow'])[0].replace(".", "_dot_")
+                    run_id = "__".join(["trig", uid, str(uuid.uuid4())])
+                    payload = {'uid': uid, 'run_id': run_id}
+                    session = settings.Session()
+                    dr = DagRun(dag_id=dag_id,
+                                run_id=run_id,
+                                conf=payload,
+                                execution_date=datetime.now(),
+                                external_trigger=True)
+                    session.add(dr)
+                    session.commit()
+                    session.close()
+                    logger.info(f"""Trigger {plugin["ptype"]} ({dag_id}) plugin for {uid}:\nrun_id: {run_id}""")
+                except SQLAlchemyError as err:
+                    logger.error(f"""Failed to trigger {plugin["ptype"]} ({dag_id}) plugin for {uid}\n {err}""")
